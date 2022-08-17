aa797d2f2cd43a3d5feb82157f46e21ab65c8792
Derriel Collins <derrielcollins96@gmail.com>
2022-08-17 01:03:37 -0500

    most of the basic controls and classes are built!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-canvas.width = 1554;
-canvas.height = 900;
+canvas.width = 1054;
+canvas.height = 850;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 // const gravity = 0.7;
@@ -10,7 +10,7 @@ const startline = new Sprite({
 		x: 0,
 		y: 700
 	},
-	width: 1564,
+	width: 1054,
 	height: 5
 });
 
@@ -23,7 +23,7 @@ const startline = new Sprite({
 // 	height: 5
 // });
 
-const frog = new SwampCreature({
+const tadpole = new Animal({
 	position: {
 		x: 750,
 		y: 600
@@ -31,6 +31,20 @@ const frog = new SwampCreature({
 	width: 100,
 	height: 100,
 	color: 'green',
+	velocity: {
+		x: 0,
+		y: 0
+	}
+});
+
+const croc = new Animal({
+	position: {
+		x: 550,
+		y: 600
+	},
+	width: 100,
+	height: 100,
+	color: 'darkgreen',
 	velocity: {
 		x: 0,
 		y: 0
@@ -49,11 +63,28 @@ const fly = new bug({
 		x: 0,
 		y: 0
 	}
-})
+});
 
-const wallCollision = (frog, wall) => {
+const redBeetle = new Bigbug({
+	position: {
+		x: 400,
+		y: 500,
+	}, 
+	width: 15,
+	height: 15,
+	color: 'red',
+	velocity: {
+		x: 0,
+		y: 0
+	}
+});
 
-}
+// const wallCollision = (frog) => {
+// 	 if (frog.position.x <= 0 || frog.position.x >= 1554)
+// 	 		{
+// 	 			frog.velocity.x = 0;
+// 	 		}
+// }
 
 
 // function randomNumber(randomNum) {
@@ -64,20 +95,107 @@ const wallCollision = (frog, wall) => {
 // 	}
 // }
 
+// player movement
+const keys = {
+	ArrowUp: {
+		pressed: false
+	},
+	ArrowDown: {
+		pressed: false
+	},
+	ArrowLeft: {
+		pressed: false
+	},
+	ArrowRight: {
+		pressed: false
+	}
+}
+	let timer = 33
+	let timerId
+const decreaseTimer = () => {
+
+timerId = setTimeout(decreaseTimer, 1000)
+	if (timer > 0) {
+		timer--
+		document.querySelector('#timer').innerHTML = timer;
+	}
+	if (timer === 0){
+		if (tadpole.points <= 25) {
+			//end game
+			clearTimeout(timerId);
+	    document.querySelector('#level-up').style.display = 'flex';
+			document.querySelector('#level-up').innerHTML = 'You Evolved!'
+			//add to speed by5
+			//start points over
+			//add swamp createrue
+		} else if (tadpole.points === 50) {
+
+		} else if (tadpole.points === 75) {
+
+		}
+	}
+
+}
+// let animalMovement = (animal) => {
+// 	console.log(animal)
+// 	if (keys.ArrowUp.pressed && animal.lastKey ==='ArrowUp'){
+// 		animal.velocity.y = -5
+// 	}
+// }
+decreaseTimer();
+
+
  function animate() {
  	window.requestAnimationFrame(animate)
  	c.fillStyle = 'black';
  	c.fillRect(0,0, canvas.width, canvas.height);
- 	frog.update();
+ 	tadpole.update();
+ 	croc.start = false;
+ 	tadpole.start = true;
  	fly.update();
+ 	redBeetle.update();
  	startline.update();
+ 	// animalMovement(t);
 
  	// frog.velocity.y = -10;
- 	console.log(fly.position.y)
- 	console.log(fly.position.x)
-  console.log(frog.position.y)
- 	console.log(frog.position.x)
+ 	// wallCollision(frog);
 
+  // console.log(frog.position.y)
+ 	// console.log(frog.position.x)
+ 	//FROG MOVEMENT I can not refactor this into a function,
+ 	//i think because of how i am referencing the objects
+ 
+ 	tadpole.velocity.x = 0;
+ 	tadpole.velocity.y = 0;
+ 	if (keys.ArrowUp.pressed && tadpole.lastKey ==='ArrowUp'){
+		tadpole.velocity.y = -5
+	} else if (keys.ArrowDown.pressed && tadpole.lastKey ==='ArrowDown') {
+		tadpole.velocity.y = 5
+	} else if (keys.ArrowLeft.pressed && tadpole.lastKey ==='ArrowLeft') {
+		tadpole.velocity.x = -5
+	} else if (keys.ArrowRight.pressed && tadpole.lastKey ==='ArrowRight'){
+		tadpole.velocity.x = 5
+	}
+
+	//croc movement
+	croc.velocity.x = 0;
+ 	croc.velocity.y = 0;
+ 	if (keys.ArrowUp.pressed && croc.lastKey ==='ArrowUp'){
+		croc.velocity.y = -10
+	} else if (keys.ArrowDown.pressed && croc.lastKey ==='ArrowDown') {
+		croc.velocity.y = 10
+	} else if (keys.ArrowLeft.pressed && croc.lastKey ==='ArrowLeft') {
+		croc.velocity.x = -10
+	} else if (keys.ArrowRight.pressed && croc.lastKey ==='ArrowRight'){
+		croc.velocity.x = 10
+	}
+
+if (tadpole.points >= 25) {
+			//end game
+	    document.querySelector('#level-up').style.display = 'flex';
+			document.querySelector('#level-up').innerHTML = 'You Evolved!';
+			timer = 33
+	}
  
 }
 
@@ -86,36 +204,66 @@ animate();
 
 window.addEventListener('keydown', (event) => {
 	console.log(event.key)
+if (tadpole.start){
 	switch (event.key) {
 		case 'ArrowUp':
-		frog.velocity.y = -5
+		keys.ArrowUp.pressed = true
+		tadpole.lastKey = 'ArrowUp'
 		break
 		case 'ArrowLeft':
-		frog.velocity.x = -5
+		keys.ArrowLeft.pressed = true
+		tadpole.lastKey = 'ArrowLeft'
 		break
 		case 'ArrowRight':
-		frog.velocity.x = 5
+		keys.ArrowRight.pressed = true
+		tadpole.lastKey = 'ArrowRight'
 		break
 		case 'ArrowDown':
-		frog.velocity.y = 5
+		keys.ArrowDown.pressed = true
+		tadpole.lastKey = 'ArrowDown'
 		break
 	}
+}
+if (croc.start) {
+	switch (event.key){
+		case 'ArrowUp':
+		keys.ArrowUp.pressed = true
+		croc.lastKey = 'ArrowUp'
+		break
+		case 'ArrowLeft':
+		keys.ArrowLeft.pressed = true
+		croc.lastKey = 'ArrowLeft'
+		break
+		case 'ArrowRight':
+		keys.ArrowRight.pressed = true
+		croc.lastKey = 'ArrowRight'
+		break
+		case 'ArrowDown':
+		keys.ArrowDown.pressed = true
+		croc.lastKey = 'ArrowDown'
+		break
+	}
+}
 })
 
 window.addEventListener('keyup', (event) => {
 	console.log(event.key)
 	switch (event.key) {
 		case 'ArrowUp':
-		frog.velocity.y = 0
+		keys.ArrowUp.pressed = false
 		break
 		case 'ArrowLeft':
-		frog.velocity.x = 0
+		keys.ArrowLeft.pressed = false
 		break
 		case 'ArrowRight':
-		frog.velocity.x = 0
+		keys.ArrowRight.pressed = false
 		break
 		case 'ArrowDown':
-		frog.velocity.y = 0
+		keys.ArrowDown.pressed = false
 		break
 	}
+})
+
+window.addEventListener('click', (event) => {
+	console.log(event);
 })
--- js/classes.js
+++ js/classes.js
@@ -17,16 +17,18 @@ class Sprite {
 }
 
 
-class SwampCreature extends Sprite {
+class Animal extends Sprite {
 	constructor({position, width, height, color, velocity}){
 		super({
-
+			position,
+			width,
+			height,
 		})
-		this.position = position;
-		this.width = width;
-		this.height = height;
 		this.color = color;
 		this.velocity = velocity;
+		this.points = 0;
+		this.lastKey;
+		this.start;
 	}
 	draw(){
 		c.fillStyle = this.color;
@@ -37,15 +39,30 @@ class SwampCreature extends Sprite {
 		//this is the base algorithom for player movement
 	    this.position.y += this.velocity.y;
 	    this.position.x += this.velocity.x;
+
+	    if (this.position.x + this.velocity.x <= 0)
+	 	{
+	 		this.velocity.x = 0;
+	 		this.position.x = 0;
+	 	} else if (this.position.x + this.width + this.velocity.x >= 1054){
+	 		this.velocity.x = 0;
+	 		this.position.x = 954;
+	 	} else if (this.position.y + this.velocity.y <= 0){
+	 		this.velocity.y = 0;
+	 		this.position.y = 0;
+	 	} else if (this.position.y + this.height + this.velocity.y >= 850) {
+	 		this.velocity.y = 0;
+	 		this.position.y = 750;
+	 	}
 	}
 
 }
 
 function getRandomX() {
-  return getRandomInt(0, 250) * 5; // Returns 10, 20, 30, 40 or 50
+  return getRandomInt(0, 150) * 5; // Returns multipls of 5
 }
 function getRandomY() {
-  return getRandomInt(0, 150) * 5; // Returns 10, 20, 30, 40 or 50
+  return getRandomInt(0, 150) * 5; // Returns multipls of 5
 }
 
 function getRandomInt(min, max) {
@@ -54,7 +71,7 @@ function getRandomInt(min, max) {
 
 // let randomX = Math.round(Math.floor(Math.random() * 1501));
 // let randomY = Math.round(Math.floor(Math.random() * 901));
-class bug extends SwampCreature {
+class bug extends Animal {
 	constructor({position, width, height, color, velocity}){
 		super({position, width, height, color, velocity})
 	}
@@ -64,20 +81,72 @@ class bug extends SwampCreature {
 	}
 	update() {
 		this.draw();
-		//serves as a start position
-	    this.position.y += this.velocity.y;
-	    this.position.x += this.velocity.x;
-	    	if (
- 			this.position.x > frog.position.x 
+		//this is the 
+	    // this.position.y += this.velocity.y;
+	    // this.position.x += this.velocity.x;
+	    //frog eats bug
+	    if (
+ 			this.position.x > tadpole.position.x 
  			&& 
- 			this.position.x < frog.position.x + frog.width
+ 			this.position.x < tadpole.position.x + tadpole.width
  			&&
- 			this.position.y > frog.position.y
+ 			this.position.y > tadpole.position.y
  			&& 
- 			this.position.y < frog.position.y + frog.height
+ 			this.position.y < tadpole.position.y + tadpole.height
  		) {
  		this.position.x = getRandomX();
  		this.position.y = getRandomY();
+ 		tadpole.points += 3;
+ 		document.querySelector('#display-points').innerHTML = tadpole.points
+ 		}
+ 		//croc eats bug
+ 		if (
+ 			this.position.x > croc.position.x 
+ 			&& 
+ 			this.position.x < croc.position.x + croc.width
+ 			&&
+ 			this.position.y > croc.position.y
+ 			&& 
+ 			this.position.y < croc.position.y + croc.height
+ 		) {
+ 		this.position.x = getRandomX();
+ 		this.position.y = getRandomY();
+ 		croc.points += 1;
+ 		document.querySelector('#display-points').innerHTML = croc.points
+ 		}
+	}
+
+
+}
+
+class Bigbug extends bug {
+	constructor({position, width, height, color, velocity}){
+		super({position, width, height, color, velocity})
+	}
+	draw(){
+		c.fillStyle = this.color;
+		c.fillRect(this.position.x, this.position.y, this.width, this.height)
+	}
+	update() {
+		this.draw();
+		//this is the 
+	    // this.position.y += this.velocity.y;
+	    // this.position.x += this.velocity.x;
+	    if (
+ 			this.position.x > tadpole.position.x 
+ 			&& 
+ 			this.position.x < tadpole.position.x + tadpole.width
+ 			&&
+ 			this.position.y > tadpole.position.y
+ 			&& 
+ 			this.position.y < tadpole.position.y + tadpole.height
+ 		) {
+ 		this.position.x = getRandomX();
+ 		this.position.y = getRandomY();
+ 		tadpole.points += 5;
+ 		document.querySelector('#display-points').innerHTML = tadpole.points
+ 		} else {
+
  		}
 	}
 
